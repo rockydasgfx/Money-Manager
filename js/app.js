f87e6d8a80b3dafcaf96e566bfbcf6753bfa8714
unknown <rockydasgfx@gmail.com>
2022-04-13 20:59:28 +0600

    add income

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+function income() {
+  const incomeInput = document.getElementById("income");
+  const getIncomeText = incomeInput.value;
+  const income = parseInt(getIncomeText);
+  return income;
+}
+// calculate Btn event hendlar
 document.getElementById("calculateBtn").addEventListener("click", function () {
-  console.log("cal c");
+  const balance = document.getElementById("balance");
 });
 
-document.getElementById("saveBtn").addEventListener("click", function () {
-  console.log("save c");
-});
+// save Btn event hendlar
+document.getElementById("saveBtn").addEventListener("click", function () {});
