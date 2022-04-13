d8fc515c149b96dd53ca26361a201f90aa14ce19
unknown <rockydasgfx@gmail.com>
2022-04-13 20:13:17 +0600

    secund commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("js connected");
+document.getElementById("calculateBtn").addEventListener("click", function () {
+  console.log("cal c");
+});
+
+document.getElementById("saveBtn").addEventListener("click", function () {
+  console.log("save c");
+});
